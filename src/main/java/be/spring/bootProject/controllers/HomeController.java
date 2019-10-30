ff1334716bf8103d1e5add1c9101bda81303edcd
GOger <GOger@DESKTOP-3830PTT>
2019-10-30 23:34:44 +0100

    init commit

---
+++ src/main/java/be/spring/bootProject/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package be.spring.bootProject.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/home")
+	public String get() {
+		return "Welcome home !";
+	}
+}
